fb114b8d42ca915262eb56c2453db9f0bb8b0168
Mario Borisov <mario@telclic.net>
2025-02-03 18:33:50 +0000

    store fetched data to limit API calls

--- README.md
+++ README.md
@@ -1,25 +1,37 @@
 # sct-auto
 Sets your screen colour temperature based on the sunset and sunrise times from your approximate geo-location.
 Currently relies on two web services:
-    - ipinfo.io for getting the local geo-location
+    - ipinfo.io for getting your geo-location
     - sunrisesunset.io to get local sunrise and sunset times for today
 
 ### Background
-Some Linux distributions do not have a built-in night mode or night light which allows to change your screen colour temp every night
-to warmer colours making it easier on the eyes. I found the only program that works well in setting the colour temperature on the Linux 
-disto that I use is called `sct` and it is very light-weight but does not have an automatic mode based on sunrise and sunset times.
+Some Linux distributions do not have a built-in night mode or night light which allows to change your screen colour temp every night to warmer colours making it easier on the eyes. `sct` short for screen colour temperature allows that but does not have an automatic mode based on sunrise and sunset times.
 Other programs such as `qredshift` do provide it but are more resource intensive and may introduce odd screen tearing every couple of seconds.
-`sct-auto` wrapper script tries to expand `sct` making it more useful.
 
 ### Install dependencies
+```sh
 apt update && apt install curl jq sct -y
+```
 
 ### Run
+```sh
 chmod +x sct-auto.sh
 ./sct-auto.sh
+```
 
 ### Automation
-As a further development, the script will download the list of future sunsrise & sunset times in a file once and 
-all subsequent runs will read from the file eliminating the need to call the API every day. 
-The script can be added to crontab to run once per day in order to automate the process of setting the screen temperature.
+The script will store your location with the local sunsrise & sunset times in a file and update the data once a day.
+The script can be added to crontab to run every half hour in order to automate the process of setting the screen temperature.
 
+```sh
+crontab -e
+*/30 * * * * /repo-path/sct-auto.sh
+```
+
+### Tweaks
+`sct` sets the screen's color temperature in a range from 1000 to 10000 using the Kelvin scale. You can edit the default values in the script to your own preference.
+
+### References
+https://manpages.ubuntu.com/manpages/bionic/en/man1/sct.1.html
+https://ipinfo.io/
+https://sunrisesunset.io/
--- sct-auto.sh
+++ sct-auto.sh
@@ -1,24 +1,57 @@
 #!/bin/bash
 
-# Sets the screen colour temp using the `sct` tool based on local sunrise and sunset times
+# Sets the screen colour temp automatically based on your local sunrise and sunset times
 
-# call to get approximate location coordinates
-ip_json_data=$(curl -s -S "https://ipinfo.io/json") > /dev/null
-loc=$(jq -r '.loc' <<<"$ip_json_data")
+DATE_TODAY=$(date +%Y-%m-%d) # yyyy-mm-dd
+UNIX_TIME_NOW=$(date -u +%s)
+LOC_FILE=.data/current_location
+SUN_TIMES_FILE=.data/sun_times
 
-LATITUDE=$(echo $loc | cut -d "," -f1)
-LONGITUDE=$(echo $loc | cut -d "," -f2)
+get_location () {
+    mkdir -p .data
+    # call to get approximate location coordinates
+    ip_json_data=$(curl -s -S "https://ipinfo.io/json") > /dev/null
+    loc=$(jq -r '.loc' <<<"$ip_json_data")
+    
+    echo $loc > $LOC_FILE
+    get_sun_times
+}
 
-# call to get today's sunrise and sunset times
-sunrisesunset_json_data=$(curl -s -S "https://api.sunrisesunset.io/json?lat=${LATITUDE}&lng=${LONGITUDE}&time_format=unix") > /dev/null
+get_sun_times () {
+    # call to get today's sunrise and sunset times
+    lat=$(cat $LOC_FILE | cut -d "," -f1)
+    lon=$(cat $LOC_FILE | cut -d "," -f2)
+    sunrisesunset_json_data=$(curl -s -S "https://api.sunrisesunset.io/json?lat=${lat}&lng=${lon}&time_format=unix") > /dev/null
+    sunrise=$(jq -r '.results.sunrise' <<<"$sunrisesunset_json_data")
+    sunset=$(jq -r '.results.sunset' <<<"$sunrisesunset_json_data")
+    dawn=$(jq -r '.results.dawn' <<<"$sunrisesunset_json_data")
+    dusk=$(jq -r '.results.dusk' <<<"$sunrisesunset_json_data")
 
-SUNRISE_LOCAL=$(jq -r '.results.sunrise' <<<"$sunrisesunset_json_data")
-SUNSET_LOCAL=$(jq -r '.results.sunset' <<<"$sunrisesunset_json_data")
+    echo "$sunrise,$sunset,$dawn,$dusk" > $SUN_TIMES_FILE
+}
+
+if ! [ -s "$LOC_FILE" ]; then
+    # file not present or is empty so get the location
+    get_location
+fi
+
+if [ "$DATE_TODAY" != $(date -r $LOC_FILE +%Y-%m-%d) ]; then
+    echo "date today not equal to date modified so refresh the data"
+    get_location
+fi
+
+SUNRISE_LOCAL=$(cat $SUN_TIMES_FILE | cut -d "," -f1)
+SUNSET_LOCAL=$(cat $SUN_TIMES_FILE | cut -d "," -f2)
+DAWN_LOCAL=$(cat $SUN_TIMES_FILE | cut -d "," -f3)
+DUSK_LOCAL=$(cat $SUN_TIMES_FILE | cut -d "," -f4)
 
 # Set screen color temp based on time
-UNIX_TIME_NOW=$(date -u +%s)
-if [ "$UNIX_TIME_NOW" -ge $SUNRISE_LOCAL ] && [ "$UNIX_TIME_NOW" -lt $SUNSET_LOCAL ]; then
+if [ "$UNIX_TIME_NOW" -ge $DAWN_LOCAL ] && [ "$UNIX_TIME_NOW" -lt $SUNRISE_LOCAL ]; then
+    sct 5000  # dawn
+elif [ "$UNIX_TIME_NOW" -ge $SUNRISE_LOCAL ] && [ "$UNIX_TIME_NOW" -lt $SUNSET_LOCAL ]; then
     sct 6500  # daylight
+elif [ "$UNIX_TIME_NOW" -ge $SUNSET_LOCAL ] && [ "$UNIX_TIME_NOW" -lt $DUSK_LOCAL ]; then
+    sct 4500  # dusk
 else
     sct 3500  # night
 fi
